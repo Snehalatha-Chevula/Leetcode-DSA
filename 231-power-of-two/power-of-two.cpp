e3e173f3084eedb09a8771c4d6bc45e133058f42
Snehalatha-Chevula <153907085+Snehalatha-Chevula@users.noreply.github.com>
2025-08-12 11:17:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (18.07%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
-    bool isPowerOfTwo(int n) {
-        if(n < 1)
+    bool isPowerOfTwo(long n) {
+        if(n == 0)
             return false;
-        for(int i=0;i<32;i++){
-            if(n == (1<<i))
-                return true;
-        }
-        return false;
+        return ((n&(n-1)) == 0);
     }
 };
